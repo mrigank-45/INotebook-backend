30bda6b5b61095c53ff6502bfc4d2d407ecce693
mrigank-45 <mrigankshukla2015@gmail.com>
2022-11-12 01:30:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors')
+require("dotenv").config();
+
+connectToMongo();
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.listen(process.env.port || 7700, () => {
+    console.log(`You are connected!!`);
+})
