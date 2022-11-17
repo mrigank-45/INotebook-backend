d8fdce63bea1cb2c91625b73e39abc7adbb93594
mrigank-45 <mrigankshukla2015@gmail.com>
2022-11-17 23:36:43 +0530

    mondoAtlas connection

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017"
+require("dotenv").config();
 
 
 const connectToMongo = () => {
     // mongoose.connect(mongoURI, () => {
     //     console.log("Connected to Mongo Successfully");
     // })
-    mongoose.connect('mongodb://localhost/inotebook', { useNewUrlParser: true });
-
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 }
 module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.listen(process.env.port || 7700, () => {
+app.listen(process.env.PORT || 7700, () => {
     console.log(`You are connected!!`);
 })
